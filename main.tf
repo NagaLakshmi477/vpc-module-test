87474fff9b40b7f5f13c195430121878c405772e
NagaLakshmi477 <jaishikareddy997@gmail.com>
2025-08-23 13:15:45 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-  source = "../terraform-aws-vpc" #https://github.com/NagaLakshmi477/terraform-aws-vpc.git
+  source = "../terraform-aws-vpc"  
   environment = var.environment
   project = var.project
   public_subnet_cidrs = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
   database_subnet_cidrs = var.database_subnet_cidrs
-  #is_peering_required = true
+  is_peering_required = true
 }
